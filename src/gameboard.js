2bd9d6f40920fc1f42498a9422201f5c9f1e1283
Sally <143855164+SallyCunningham620@users.noreply.github.com>
2026-01-26 14:34:35 -0600

    updated some tests with new isValidCoor., check validity in placeShip, update receiveAttack

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,30 +1,65 @@
-import Ship from './ship.js';
+import Ship from '../src/ship.js';
 
-const Gameboard = (size = 10) => {
-    const board = Array(size).fill(null).map(() => Array(size).fill(null));
+const Gameboard = () => {
+    const board = Array(10).fill(null).map(() => Array(10).fill(null));
+    const missedAttacks = [];
     const ships = [];
 
+    const isValidCoordinate = (x, y) =>  x >= 0 && x < 10 && y >= 0 && y < 10;
+
     const placeShip = (shipLength, startX, startY, isVertical = false) => {
+        //check if valid
+        for (let i = 0; i < shipLength; i++) {
+            const x = isVertical ? startX + i : startX;
+            const y = isVertical ? startY : startY + i;
+
+            if (!isValidCoordinate(x, y) || board[x][y] !== null){
+                return false; //Invalid placement
+            }
+        } 
         const newShip = Ship(shipLength);
-        if (isVertical) {
-            for (let i = 0; i < shipLength; i++) {
-                if (startY + i >=size || board[startX][startY + i] !== null) return false;
-                board[startX][startY + i] = { ship: newShip, index: i };
-            }
-        } else {
-            for (let i = 0; i < shipLength; i++) {
-                if(startX + i >= size || board[startX + i][startY] !== null) return false;
-                board[startX + i][startY] = { ship: newShip, index: i};
-            }
+        ships.push(newShip);
+        //console.log(ships);
+        //place ship on board
+        for (let i = 0; i < shipLength; i++) {
+            const x = isVertical ? startX + i : startX;
+            const y = isVertical ? startY : startY + i;
+            board[x][y] = newShip;
         }
-    ships.push(newShip);
     return true;
+    };
+
+    const receiveAttack = (x, y) => {
+        if (!isValidCoordinate(x, y)) return "Invalid coordinate";
+
+        const target = board[x][y];
+
+        const alreadyMissed = missedAttacks.some(m => m[0] === x && m[1] === y);
+        const isHit = target !== null && target !== 'miss' && target.getHits() > 0;
+        if (alreadyMissed || isHit) return 'Already atacked';
+
+        if (target === null) {
+            board[x][y] = 'miss';
+            missedAttacks.push({x, y});
+            return "Miss"; // Miss
+        } else {
+            target.hit();
+            return "Hit"; // Hit
+        }
+    };
+
+    const allShipsSunk = () => {
+        return ships.length > 0 && ships.every(ship => ship.isSunk());
     };
 
     return {
         placeShip,
+        receiveAttack,
+        allShipsSunk,
+        board,
         getBoard: () => board,
-    }
-}
+        getMissedAttacks: () => missedAttacks,
+    };
+};
 
 export default Gameboard;
